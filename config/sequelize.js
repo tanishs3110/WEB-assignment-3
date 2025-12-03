a14a39c0980106604bf8cfef7581cc3d99ec3293
tanishs3110 <tanishsoni3110@gmail.com>
2025-12-02 21:23:43 -0500

    sql commit

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,17 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-let sequelize;
-
-if (!global._sequelize) {
-  global._sequelize = new Sequelize(process.env.PG_URL, {
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: {
-      ssl: { rejectUnauthorized: false }
+const sequelize = new Sequelize(process.env.PG_URL, {
+  dialect: "postgres",
+  dialectModule: require("pg"), 
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
     }
-  });
-}
-
-sequelize = global._sequelize;
+  }
+});
 
 module.exports = sequelize;
