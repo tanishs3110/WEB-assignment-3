e1ffd8afb10ea885ea2ac046886ba04881df0c51
tanishs3110 <tanishsoni3110@gmail.com>
2025-12-02 23:04:38 -0500

    vercel commit2

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
 
 
 app.use(
