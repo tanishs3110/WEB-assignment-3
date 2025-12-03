d48dfb936fa3a3bc5b9c000a2eeb6476ba985f55
tanishs3110 <tanishsoni3110@gmail.com>
2025-12-02 20:48:25 -0500

    vercel commit 2

--- config/sequelize.js
+++ config/sequelize.js
@@ -2,7 +2,12 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.PG_URL, {
   dialect: "postgres",
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
